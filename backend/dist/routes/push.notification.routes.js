8ecb432af0d71a07dc6d72cd0158a88bc1be0866
rohitsingh <rohitsingh986491@gmail.com>
2026-07-29 23:16:22 +0530

    removed the dist folder

--- backend/dist/routes/push.notification.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { isAuthenticated } from "../middlewares/isAuthenticated.js";
-import { savePushTokenController, deletePushNotificationToken } from "../controllers/push.notification.controllers.js";
-const pushRouter = express.Router();
-pushRouter.post("/push-token", isAuthenticated, savePushTokenController);
-pushRouter.delete("/push-token", isAuthenticated, deletePushNotificationToken);
-export default pushRouter;
-//# sourceMappingURL=push.notification.routes.js.map
